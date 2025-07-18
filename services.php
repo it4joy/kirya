d3525d6b92cecabd9ef5a39b9a67286c46e47a23
Igor Ivanov <drkierkegor@gmail.com>
2025-07-18 10:25:16 +0300

    Upd content
    Req by July 17, 2025.

--- services.php
+++ services.php
@@ -14,7 +14,8 @@
                             <div class="col-md-12">
                                 <div class="services-important-notes-wrapper">
                                     <!--<h5>ООО рыбхоз «Киря» предоставляет следующие услуги:</h5>-->
-                                    <h5>Платная рыбалка завершена.</h5>
+                                    <!-- <h5>Платная рыбалка завершена.</h5> -->
+									 <h5>Платная рыбалка открыта.</h5>
 									<!-- <h5>С xx июня 2025 г. открывается платная рыбалка!</h5> -->
                                     <p>
                                         <a href="#pay-fishing-details" class="fancybox-modal btn btn-small btn-blue"><i class="fa fa-info-circle"></i> Узнать подробнее</a>
